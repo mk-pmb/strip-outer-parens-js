af1e09943eed8bb379253240f18b5e5274ed4121
mk-pmb <mk-pmb@users.noreply.github.com>
2016-09-07 03:10:36 +0200

    v0.1.2: clarify tests

--- test.js
+++ test.js
@@ -2,49 +2,88 @@
 /* -*- tab-width: 2 -*- */
 'use strict';
 
-var soPar = require('strip-outer-parens'), x = 0,
+var soPar = require('strip-outer-parens'), x, s,
   eq = require('assert').deepStrictEqual;
+
 function f() { return 5; }
 
-(function justDefaults() {
-  eq(soPar('<b>hello</b>'),
-            'b>hello</b');
-  eq(soPar('((a === 1) && (b === 2))'),
-             'a === 1) && (b === 2');
-  eq(soPar('([(!((foo("<b>") instanceof Bar))])'),
-              '!((foo("<b>") instanceof Bar)');
-}());
+f.allTests = [
 
-(function parserRequiredToGuessMeaning() {
-  eq(true,  (-1 < x[")("]) || (f(/></) > 2));
-  eq(soPar('(-1 < x[")("]) || (f(/></) > 2)'),
-            '-1 < x[")("]) || (f(/></) > 2');
-}());
+  function justDefaults() {
+    x = '<b>hello</b>';
+    s =  'b>hello</b';
+    eq(s, soPar(x));
 
-(function keepSomeOuterBrackets() {
-  var opts = { keepOuter: 2 };
-  eq(soPar('{(<[[ ++ ]>)}',     opts), '{([ ++ )}');
-  eq(soPar('<>()',              opts), '<>()');
-  eq(soPar('[<>()]',            opts), '[<>()]');
-  opts = { keepOuter: 0 };
-  eq(soPar('[<>()]',            opts), '<>()');
-}());
+    x = '((a === 1) && (b === 2))';
+    s =   'a === 1) && (b === 2';
+    eq(s, soPar(x));
+
+    x = '([(!((foo("<b>") instanceof Bar))])';
+    s =    '!((foo("<b>") instanceof Bar)';
+    eq(s, soPar(x));
+    f.rxCnt(s, /\(/g, /\)/g, [3, 2]);
+    f.rxCnt(x, /\(/g, /\)/g, [5, 4]);   // => wasn't balanced to begin with.
+  },
+
+  function parserRequiredToGuessMeaning() {
+    eq(true,  (-1 < x[")("]) || (f(/></) > 2));
+    eq(soPar('(-1 < x[")("]) || (f(/></) > 2)'),
+              '-1 < x[")("]) || (f(/></) > 2');
+  },
+
+  function keepSomeOuterBrackets() {
+    var opts = { keepOuter: 2 };
+    x = f.noSpace('  {(<[[++]>)}  ');
+    s = f.noSpace('  {(  [++  )}  ');
+    eq(soPar(x, opts), s);
+
+    x = f.noSpace('  <>()  ');
+    s = f.noSpace('  <>()  ');
+    eq(soPar(x, opts), s);
+
+    x = f.noSpace('  [<>()]  ');
+    s = f.noSpace('  [<>()]  ');
+    eq(soPar(x, opts), s);
+
+    opts = { keepOuter: 0 };
+    x = f.noSpace('  [<>()]  ');
+    s = f.noSpace('   <>()   ');
+    eq(soPar(x, opts), s);
+  },
 
 
-(function customBracketPairs() {
-  var unequalPairs = [
-    ['<<', '>'],
-    ['[', ']]'],
-  ], opts = { keepOuter: 0, pairs: unequalPairs };
-  eq(soPar('<<[[::]]>>', opts),
-             '[[::]]>');
-  opts.keepOuter = 2;
-  eq(soPar('<<[[<<::>]]]]>', opts),
-           '<<[::]]>');
-}());
+  function customBracketPairs() {
+    var unequalPairs = [
+      ['<<', '>'],
+      ['[', ']]'],
+    ], opts = { keepOuter: 0, pairs: unequalPairs };
+
+    x = f.noSpace('  << [ [ :: ]] > >  ');
+    s = f.noSpace('     [ [ :: ]] >    ');
+    eq(soPar(x, opts), s);
+
+    opts.keepOuter = 2;
+    x = f.noSpace('  << [ [ << :: > ]] ]] >  ');
+    s = f.noSpace('  << [      ::      ]] >  ');
+    eq(soPar(x, opts), s);
+  },
+
+  console.log.bind(console, '+OK tests passed')
+];
+
+
+f.rxCnt = function (t) {
+  var rxs = Array.prototype.slice.call(arguments, 1), a = rxs.pop();
+  eq(rxs.map(function (r) { return t.match(r).length; }), a);
+};
+
+f.noSpace = function (t) { return t.replace(/\s+/g, ''); };
 
 
 
 
 
-console.log('+OK tests passed');
+
+
+
+f.allTests.forEach(function (t) { t(); });
